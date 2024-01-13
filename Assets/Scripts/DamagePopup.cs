f4d044b72ed92def9ef3ec2294b29ce60e5ae39e
Vlad Sunny <vladislavsunny@gmail.com>
2024-01-13 23:38:33 +0300

    Damage text animation

--- Assets/Scripts/DamagePopup.cs
+++ Assets/Scripts/DamagePopup.cs
@@ -8,10 +8,12 @@ using Random = UnityEngine.Random; // Используйте UnityEngine.UI дл
 
 public class DamagePopup : MonoBehaviour
 {
-    [SerializeField] private float disappearTimer = 1f;
+    [SerializeField] private float lifeTime = 1f;
     [SerializeField] private Vector3 randomizePositionFrom = new Vector3(1f, 1f, 0);
     [SerializeField] private Vector3 randomizePositionTo = new Vector3(1f, 1f, 0);
+    
     private TextMeshProUGUI damageText;
+    private float disappearTimer;
 
 
     private void Start()
@@ -21,33 +23,26 @@ public class DamagePopup : MonoBehaviour
 
     private void OnEnable()
     {
-        // transform.localPosition += new Vector3(
-        //     Random.Range(randomizePositionFrom.x, randomizePositionTo.x),
-        //     Random.Range(randomizePositionFrom.y, randomizePositionTo.y),
-        //     Random.Range(randomizePositionFrom.z, randomizePositionTo.z)
-        // );
-        
         if (Camera.main != null)
         {
-            // Получаем направления камеры
             Vector3 cameraRight = Camera.main.transform.right;
             Vector3 cameraUp = Camera.main.transform.up;
             Vector3 cameraForward = Camera.main.transform.forward;
-
-            // Удаляем компонент y, если не хотим, чтобы текст перемещался вверх и вниз вдоль направления взгляда камеры
+            
             cameraForward.y = 0;
-
-            // Рандомизируем смещение вдоль векторов право, вверх и вперед камеры
+            
             transform.position += cameraRight * Random.Range(randomizePositionFrom.x, randomizePositionTo.x) 
                                   + cameraUp * Random.Range(randomizePositionFrom.y, randomizePositionTo.y)
                                   + cameraForward * Random.Range(randomizePositionFrom.z, randomizePositionTo.z);
         }
+
+        disappearTimer = lifeTime;
     }
 
     public void Setup(float damageAmount)
     {
         damageText = GetComponentInChildren<TextMeshProUGUI>();
-        damageText.text = damageAmount.ToString();
+        damageText.text = "-" + damageAmount.ToString();
     }
 
     private void Update()
